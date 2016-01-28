fd58e59f4f54202347f79b949c499d18a7a561fb
Steve Radomski <radomskist@yahoo.com>
2016-01-28 15:33:53 -0600

    Cleaning dependency folder

--- src/Viewer/NaiGL.cpp
+++ src/Viewer/NaiGL.cpp
@@ -51,7 +51,9 @@ naigl::naigl() {
 		NULL);
 
 	glEnableVertexAttribArray(0);
-
+	
+	//Creating our shaders
+	shaderinit();
 }
 
 
@@ -71,3 +73,95 @@ naigl::~naigl() {
 
 }
 
+
+void naigl::shaderinit() {
+	/*
+	TODO: Finish
+	const GLchar const* shadertest = {"#version 330 core\n"
+	"in vec4 position;\n"
+
+	"out VS_OUT\n"
+	"{"
+	" vec4 color; \n"
+	"} vs_out;"
+
+	"uniform mat4 view;\n"
+	"void main()\n"
+		"{\n"
+
+			"const vec4 colors[] = vec4[3](vec4(1.0,0.0,0.0,1.0), vec4(0.0,1.0,0.0,1.0), vec4(0.0, 0.0, 1.0, 1.0));\n"
+
+			"gl_Position.xyzw =  view * position.xyzw;"
+
+			"vs_out.color.x = sin(gl_Position.w / 6.25);\n"
+			"vs_out.color.y = sin(gl_Position.w / 15.5);\n"
+			"vs_out.color.z = sin(gl_Position.w / 39);"				
+			"}"
+	};
+
+	//NOTE: TAKEN FROM THE SUPER BIBLE
+	const GLchar const* fragmenttest = {"#version 330 core\n"
+		"out vec4 color;\n"
+
+		"in VS_OUT\n"
+		"{ vec4 color;\n"
+		"} fs_in;\n"
+
+		"void main()\n"
+		"{\n"
+		"color = fs_in.color;"
+		"}"
+	};
+
+	GLuint shaderobj;
+	GLuint fragshadeobj;
+
+	shaderobj = glCreateShader(GL_VERTEX_SHADER);  
+
+	glShaderSource(shaderobj,
+		1, 
+		shadertest,
+		NULL);
+
+	glCompileShader(shaderobj);
+
+	fragshadeobj = glCreateShader(GL_FRAGMENT_SHADER);  
+		glShaderSource(fragshadeobj, 
+		1,
+		fragmenttest,
+		NULL);
+
+	glCompileShader(fragshadeobj); 
+
+
+	GLint isCompiled = 0;
+	glGetShaderiv(shaderobj, GL_COMPILE_STATUS, &isCompiled);
+
+	if(isCompiled == GL_FALSE) {
+		glGetShaderiv(shaderobj, GL_INFO_LOG_LENGTH, &isCompiled);
+		GLchar* errormessage = new GLchar[isCompiled + 1];
+	
+		glGetShaderInfoLog(shaderobj, isCompiled, &isCompiled, errormessage);
+	
+		std::cout << "Vertex failed\n" << errormessage << std::endl << std::flush;
+	}
+
+	glGetShaderiv(fragshadeobj, GL_COMPILE_STATUS, &isCompiled);
+	if(isCompiled == GL_FALSE) {
+		glGetShaderiv(fragshadeobj, GL_INFO_LOG_LENGTH, &isCompiled);
+		GLchar* errormessage = new GLchar[isCompiled + 1];
+	
+		glGetShaderInfoLog(fragshadeobj, isCompiled, &isCompiled, errormessage);
+	
+		std::cout << "Fragment failed\n" << errormessage << std::endl << std::flush;
+	}
+
+	naishader = glCreateProgram();
+	glAttachShader(naishader, shaderobj);
+	glAttachShader(naishader, fragshadeobj); 
+	glLinkProgram(naishader); 
+
+	glUseProgram(naishader); 
+	*/
+
+}
