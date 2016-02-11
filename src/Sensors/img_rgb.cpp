06c7e584cbf30dc30d80131bcce0d47411dd4ba1
Steve Radomski <radomskist@yahoo.com>
2016-02-10 20:57:39 -0600

    Fixed huge memory leak

--- src/Sensors/img_rgb.cpp
+++ src/Sensors/img_rgb.cpp
@@ -23,13 +23,13 @@ void imgrgb::ProcessImg(unsigned char *rgbbuff) {
 	img = rgbin(cv::Rect(120, 0, 1539, 1080));
 	cv::resize(img,rgbin, cv::Size(512,400));
 	cv::cvtColor(rgbin,rgbin,CV_BGRA2BGR);
+	cv::bilateralFilter(rgbin, img, 4, 300, 300);
 
-	cv::bilateralFilter(rgbin, img, 4, 150, 150);
 	cv::Mat cannystuff(512,400,CV_8UC1);
 	//cv::morphologyEx(rgbin, img, cv::MORPH_CLOSE, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5)));
-	cv::morphologyEx(img, img, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(20, 20)));
+	//cv::morphologyEx(img, img, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_RECT, cv::Size(20, 20)));
 
-	cv::Canny(img, cannystuff, 80, 80, 3, false); //Detecting edges
+	cv::Canny(img, cannystuff, 40, 40, 3, false); //Detecting edges
 	//std::vector<cv::Vec3f> circlelist;
 	//cv::HoughCircles(cannystuff, circlelist, CV_HOUGH_GRADIENT, 3, 8);
 	//cv::Mat matcircle = cv::Mat::zeros(512,400,CV_8UC1);
@@ -43,6 +43,8 @@ void imgrgb::ProcessImg(unsigned char *rgbbuff) {
 		krgb.data[i*4 + 1] = img.data[i*3 + 1];
 		krgb.data[i*4 + 2] = img.data[i*3 + 2];	
 		krgb.data[i*4 + 3] = 0;	
+		if(cannystuff.data[i])
+			krgb.data[i*4] = cannystuff.data[i];
 	}
 	img.release();
 	rgbin.release();
