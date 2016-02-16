5eea5be1fc2b071fea1fc48b59a4376472f7036f
Steve Radomski <radomskist@yahoo.com>
2016-02-16 00:01:25 -0600

    Added connection gnerating script

--- connectrun.sh
+++ connectrun.sh
@@ -1,11 +1,7 @@
 #!/usr/expect
-
 spawn ssh -t -Y -C niurobotics@10.42.0.1 "cd /home/niurobotics/nai/;sudo ./NaiBrain"
-
-#ugly usage of cat
-expect "password:"
+expect "password:""
 send "NAI\r"
 expect "password:"
 send "NAI\r"
-interact
-$SHELL
+"interact"
--- connectruns.h
+++
@@ -1,7 +0,0 @@
-#!/usr/expect
-spawn ssh -t -Y -C niurobotics@10.42.0.1 "cd /home/niurobotics/nai/;sudo ./NaiBrain"
-expect "password:""
-send "NAI"
-expect "password:"
-send "NAI"
-"interact"
